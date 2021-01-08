7a93d4bd383aee7796c80fbeda57f85257a92afc
轩乾辉 <xuanqianhui@birdsh.com>
2021-01-08 14:46:06 +0800

    优化

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { HashRouter, Route, Switch} from 'react-router-dom';
-import App from "../App"
+import App from "../App";
+import Login from "../views/login/login";
 import {Redirect} from 'react-router-dom'
 
 
@@ -11,6 +12,8 @@ export default class RouteConfig extends Component{
         <Switch>
           <Route exact path="/">
             <Redirect to="/jobManager" />
+          </Route>
+          <Route exact path="/login" component={Login}>
           </Route>
           <App/>
         </Switch>
